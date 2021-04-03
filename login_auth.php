94af12e09007c83e85f0b8a01070eb729a85863d
Surya Hebbar <suryahebbar007@gmail.com>
2021-04-03 18:54:56 +0530

    Replaced index page with login

--- login_auth.php
+++ login_auth.php
@@ -7,7 +7,7 @@
 		if(login($conn,$email_id,$passwordhash)){
 			echo "<h2> User Logged in successfully! Go back to index page -> <a href=\"index.php\">Home</a></h1><br/></h2>";
 		}else{
-			echo "<h2> Invalid username or password. Go back to login page -> <a href=\"login.html\">Login</a></h1><br/>";		
+			echo "<h2> Invalid username or password. Go back to login page -> <a href=\"index.html\">Login</a></h1><br/>";		
 		}
 	}else{
 		echo "Unknown Error!";
